7001f3612fe37c2c1f11eca63f56b0c362601d18
Vladimir Sarychev <svmyhome@mail.ru>
2020-11-30 21:06:22 +0300

    Добавлены TODAY = [] TOMORROW = [] OTHER = []

--- toDoList.py
+++ toDoList.py
@@ -1,23 +1,45 @@
+import datetime
+
+
 HELP = '''
     help  - вывести подсказки по командам
     add - добавить задачу
     print - распечатать списо задач
 '''
-TASKS = []
+TODAY = []
+TOMORROW = []
+OTHER = []
+
 start = True
 while start:
+    now_date = datetime.datetime.today().strftime("%d%m%Y")
+    tomorrow_date = (datetime.datetime.today() + datetime.timedelta(1)).strftime("%d%m%Y")
     new_command = input('Введите команду: ').lower()
     if new_command == 'help':
         print(HELP)
     elif new_command == 'add':
+        date_task = input('Введите дату в формате ddmmyyyy: ')
         new_task = input('Введите название задачи: ').lower()
-        TASKS.append(new_task)
-        print('Задача добавлена в список')
-        print('Текущий список задач', TASKS)
+        if date_task == now_date:
+                TODAY.append(new_task)
+                print('Задача добавлена в список')
+                print('Текущий список задач', TODAY)
+        elif date_task == tomorrow_date:
+                TOMORROW.append(new_task)
+                print('Задача добавлена в список')
+                print('Текущий список задач', TOMORROW)
+        elif date_task != [tomorrow_date, now_date]:
+            OTHER.append(new_task)
+            print('Задача добавлена в список')
+            print('Текущий список задач', OTHER)
     elif new_command == 'print':
         print('Текущий список задач: ')
-        for task in TASKS:
-            print(task)
+        for task in TODAY:
+            print('Список задачь текущего дня', task)
+        for task in TOMORROW:
+            print('Список задачь завтрашнего дня', task)
+        for task in OTHER:
+            print('Список задать прочие', task)
     elif new_command == 'exit' or new_command != ['add','help', 'print']:
         print('Команда не распознана введите одну из следующих команд:', HELP)
         print('Спасибо за использование! До свидания!')
