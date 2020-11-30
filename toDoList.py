937e86269c5f5dc50172a63362758c107b534292
Vladimir Sarychev <svmyhome@mail.ru>
2020-11-30 20:08:13 +0300

    Добавленр создание задачи и выход из проекта

---
+++ toDoList.py
@@ -0,0 +1,23 @@
+HELP = '''
+    help  - вывести подсказки по командам
+    add - добавить задачу
+    print - распечатать списо задач
+'''
+TASKS = []
+start = True
+while start:
+    new_command = input('Введите команду: ').lower()
+    if new_command == 'help':
+        print(HELP)
+    elif new_command == 'add':
+        new_task = input('Введите название задачи: ').lower()
+        TASKS.append(new_task)
+        print('Текущий список задач', TASKS)
+    elif new_command == 'print':
+        print('Текущий список задач', TASKS)
+    elif new_command == 'exit' or new_command != ['add','help', 'print']:
+        print('Команда не распознана введите одну из следующих команд:', HELP)
+        print('Спасибо за использование! До свидания!')
+        break
+
+ #   start = bool(input('Вы хотите ввести еще задачу(True/False)? ').title())
